9cbf002d99253e8b9625fe79132b1b1832b811fb
YasasDaraka <darakayasas@gmail.com>
2024-06-20 18:21:08 +0530

    Update

--- config-service/src/test/java/lk/ijse/gdse66/config_service/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/lk/ijse/gdse66/config_service/ConfigServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package lk.ijse.gdse66.config_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootTest
+@EnableDiscoveryClient
+@EnableConfigServer
 class ConfigServiceApplicationTests {
 
 	@Test
