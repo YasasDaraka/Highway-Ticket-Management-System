fc8f85e381bc33d9c19161cc0f9d7759b1d0ca6a
YasasDaraka <darakayasas@gmail.com>
2024-06-21 11:32:35 +0530

    Update

--- user-service/src/main/java/lk/ijse/gdse/userserver/controller/UserController.java
+++ user-service/src/main/java/lk/ijse/gdse/userserver/controller/UserController.java
@@ -3,8 +3,12 @@ package lk.ijse.gdse.userserver.controller;
 import lk.ijse.gdse.userserver.dto.UserDTO;
 import lk.ijse.gdse.userserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -17,15 +21,33 @@ public class UserController {
     @Autowired
     RestTemplate restTemplate;
 
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(path = "/getAll")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(path = "/search/{id}")
+    public UserDTO getUser(@PathVariable("id") String id) {
+        return userService.searchUser(id);
+    }
+
     @PostMapping
-    public boolean saveUser(@RequestBody UserDTO dto){
+    public ResponseEntity<Void> saveUser(@RequestBody UserDTO dto){
         userService.saveUser(dto);
-        return true;
+        return ResponseEntity.ok().build();
     }
 
-    @GetMapping
-    public String getAllUser(){
-        return "awaaa";
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(@RequestBody UserDTO dto) {
+        userService.updateUser(dto);
+        return ResponseEntity.noContent().build();
     }
 
+    @DeleteMapping(params = "userNic")
+    public ResponseEntity<Void> deleteCustomer(@RequestParam("userNic") String userNic) {
+        userService.deleteUser(userNic);
+        return ResponseEntity.noContent().build();
+    }
 }
